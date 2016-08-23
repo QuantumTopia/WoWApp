f138ec80674dcd730f7807dfd94949556794fa75
Nikiforos Rotas <n_rotas@yahoo.com>
2016-08-23 22:58:30 +0100

    Commit 2

--- app/src/main/java/com/quantumtopia/krauser/wowapp/Target.java
+++ app/src/main/java/com/quantumtopia/krauser/wowapp/Target.java
@@ -1,12 +1,33 @@
 package com.quantumtopia.krauser.wowapp;
 
 /**
- * Created by Krauser on 8/22/2016.
+ * Created by Krauser on 8/23/2016.
  */
-public class Target extends Entity
+public class Target
 {
+    private int number;
+    private int max_health;
+    private int current_health;
+    private float health_percentage;
+    private boolean alive;
+
     public Target(int health)
     {
-        super(health);
+        max_health = health;
+        number = 1;
+        alive = true;
+    }
+
+    public Target(int health, int number)
+    {
+        max_health = health;
+        this.number = number;
+        alive = true;
+    }
+
+    public boolean take_damage(int damage)
+    {
+        current_health -= damage;
+        return current_health > 0;
     }
 }
